3dfaeb44f7d513e517d13edc2f047d096201b488
bigerx <127696805+bigerx@users.noreply.github.com>
2023-04-18 02:25:53 +0200

    Update docs.py

--- frappe_system_monitor/config/docs.py
+++ frappe_system_monitor/config/docs.py
@@ -8,4 +8,4 @@ Configuration for docs
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
-	context.brand_html = "Frappe System Monitor"
+	context.brand_html = "Oko System Monitor for ERP"
